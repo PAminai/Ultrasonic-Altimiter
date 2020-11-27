cc4be994b439eec3acc81911bba19a83e8c1b623
Pouya Aminaie <Pooya.Aminai@gmail.com>
2020-11-28 01:57:21 +0330

    Add files via upload

---
+++ ultra_sonic.ino
@@ -0,0 +1,32 @@
+int PingPin=13; //transmit
+int EchoPin=12; //receave
+
+
+void setup() {
+pinMode(PingPin,OUTPUT);
+pinMode(EchoPin,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long duration,cm ;
+digitalWrite(PingPin,LOW);
+delayMicroseconds(2);
+
+digitalWrite(PingPin,HIGH);
+delayMicroseconds(5);
+
+digitalWrite(PingPin,HIGH);
+
+duration=pulseIn(EchoPin,HIGH);
+
+cm=MicroToCm(duration);
+Serial.print(cm);
+Serial.println("cm");
+
+}
+
+unsigned long MicroToCm(unsigned long Micro){
+  return(Micro/29)/2;
+}
+
